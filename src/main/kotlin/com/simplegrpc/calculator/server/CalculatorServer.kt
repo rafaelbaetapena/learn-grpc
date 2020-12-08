c4685c3f5a9a102359561718f569a719c416e6e0
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2020-12-08 11:02:23 -0300

    calculator server

--- src/main/kotlin/com/simplegrpc/calculator/server/CalculatorServer.kt
+++ src/main/kotlin/com/simplegrpc/calculator/server/CalculatorServer.kt
@@ -1,4 +1,39 @@
 package com.simplegrpc.calculator.server
 
-class CalculatorServer {
+import io.grpc.Server
+import io.grpc.ServerBuilder
+
+class CalculatorServer(private val port: Int) {
+    val server: Server = ServerBuilder
+            .forPort(port)
+            .build()
+
+    fun start(){
+        server.start()
+        println("Server started, listening on $port")
+        Runtime.getRuntime().addShutdownHook(
+                Thread {
+                    println("*** shutting down gRPC server since JVM is shutting down")
+                    this@CalculatorServer.stop()
+                    println("*** server shut down")
+                }
+        )
+    }
+
+    private fun stop() {
+        server.shutdown()
+    }
+
+    fun blockUntilShutdown() {
+        server.awaitTermination()
+    }
+}
+
+fun main() {
+    println("Hello gRPC calculator server")
+
+    val port = 50051
+    val server = CalculatorServer(port)
+    server.start()
+    server.blockUntilShutdown()
 }
