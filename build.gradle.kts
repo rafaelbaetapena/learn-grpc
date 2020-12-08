e80e3cbb8e56aa2c826481448cec014e7e812a2b
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2020-12-08 19:24:26 -0300

    add kotlinx

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ protobuf {
 dependencies {
     implementation("io.grpc:grpc-kotlin-stub:0.2.1")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
     runtimeOnly("io.grpc:grpc-netty:1.32.1")
 
     testImplementation(kotlin("test-junit5"))
